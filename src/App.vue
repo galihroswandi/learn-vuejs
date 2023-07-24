0cb026c125c6598a7270816ce25a2fddc2a7c578
galihroswandi <galihroswandi25@gmail.com>
2023-07-24 16:22:05 +0700

    feat: Video 14 Computed (#14)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  <div v-html="message"></div>
+  <div>{{ total }}</div>
+
+  <input v-model="num1" type="number" />
+  <input v-model="num2" type="number" />
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message:
-        "<span style='color: blue; font-size: 5rem;'>Dirender ulang</span>",
+      num1: 0,
+      num2: 0,
     };
+  },
+  computed: {
+    total() {
+      if (this.num1 > this.num2) {
+        return "Number 1 lebih besar dari Number 2";
+      } else {
+        return "Number 1 lebih kecil dari Number 2";
+      }
+    },
   },
 };
 </script>
