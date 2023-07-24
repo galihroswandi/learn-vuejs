8c201ce4229f5156d59492f040b7f9f85f5ea590
galihroswandi <galihroswandi25@gmail.com>
2023-07-24 14:56:56 +0700

    feat: Video 8 Component Sederhana (#8)

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <template>
-  <ul>
-    <li v-for="(todo, index) in todos" :key="todo.id">
-      {{ index + 1 + " - " + todo.activity }}
-    </li>
-  </ul>
+  <h1>Ini adalah parent component</h1>
+  <ChildComponent />
 </template>
 
 <script>
+import ChildComponent from "./components/ChildComponent.vue";
 export default {
-  data() {
-    return {
-      todos: [
-        { id: 1, activity: "Olahraga" },
-        { id: 2, activity: "Masak" },
-        { id: 3, activity: "Mandi" },
-        { id: 4, activity: "Makan" },
-      ],
-    };
-  },
+  components: { ChildComponent },
 };
 </script>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Ini adalah child component hi.</h1>
+</template>
