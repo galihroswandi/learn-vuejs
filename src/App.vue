d5386c9e2406bc49530216c519df10475d60b415
galihroswandi <galihroswandi25@gmail.com>
2023-07-24 14:22:25 +0700

    feat: Video 5 v-model (#5)

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <template>
-  <div class="wrapper" style="display: flex; align-items: center; gap: 1rem">
-    <button @click="minCount">-</button>
-    <h1>{{ count }}</h1>
-    <button @click="count++">+</button>
-  </div>
+  <h1>{{ message }}</h1>
+
+  <input v-model="message" type="text" />
+
+  <!-- v-model kependekan dari : -->
+  <input type="text" :value="message" @input="message = $event.target.value" />
 </template>
 
 <script>
 export default {
   data() {
     return {
-      count: 0,
+      message: "say hi",
     };
-  },
-  methods: {
-    minCount() {
-      if (this.count > 0) {
-        this.count--;
-      }
-    },
   },
 };
 </script>
