1db82fb8ee69c8b2c2b3dcf3903fa287dd84915d
galihroswandi <galihroswandi25@gmail.com>
2023-07-24 16:04:36 +0700

    feat: v-once (#12)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <h1 id="rerender">Tulisan {{ message }}</h1>
+  <!-- v-once artinya render satu kali jadi ketika ada perubahan komponent ini tidak akan dirender ulang -->
+  <h1 v-once>Tulisan {{ message }}</h1>
 
-  <button @click="message = 'Update !!'">Update !!</button>
+  <button @click="message = 'Updated !!'">Update !!</button>
 </template>
 
 <script>
@@ -10,42 +11,6 @@ export default {
     return {
       message: "Dirender ulang",
     };
-  },
-  beforeCreate() {
-    console.log("Before created");
-  },
-  created() {
-    console.log("Created");
-    console.log({ message: this.message });
-    console.log({ component: document.getElementById("rerender") });
-  },
-  beforeMount() {
-    console.log("Before mounted");
-  },
-  mounted() {
-    console.log("Mounted");
-    console.log({ message: this.message });
-    console.log({ component: document.getElementById("rerender") });
-  },
-  beforeUpdate() {
-    console.log("Before updated");
-    console.log({ message: this.message });
-    console.log({
-      textContent: document.getElementById("rerender").textContent,
-    });
-  },
-  updated() {
-    console.log("Updated");
-    console.log({ message: this.message });
-    console.log({
-      textContent: document.getElementById("rerender").textContent,
-    });
-  },
-  beforeUnmount() {
-    console.log("Before unmounted");
-  },
-  unmounted() {
-    console.log("Unmounted");
   },
 };
 </script>
