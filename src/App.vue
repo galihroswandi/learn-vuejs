d47a489668487da9525babd0f79f14d36e85f8a2
galihroswandi <galihroswandi25@gmail.com>
2023-07-24 15:08:12 +0700

    feat: Video 9 Props pada Component (#9)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <h1>Ini adalah parent component</h1>
-  <ChildComponent />
+  <ChildComponent text="Hallo" />
+  <ChildComponent :text="text" />
 </template>
 
 <script>
 import ChildComponent from "./components/ChildComponent.vue";
 export default {
+  data() {
+    return {
+      text: "ini component kedua.",
+    };
+  },
   components: { ChildComponent },
 };
 </script>
--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,3 +1,15 @@
 <template>
   <h1>Ini adalah child component hi.</h1>
+  <p>{{ text }}</p>
 </template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
