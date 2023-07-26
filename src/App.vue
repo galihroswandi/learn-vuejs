46d967ffaf9f668aa75491ff41cf7cc4e514a117
galihroswandi <galihroswandi25@gmail.com>
2023-07-26 11:14:03 +0700

    feat: COMPOSITION API (Video 1 Introduction & Video 2 Ref) (#31) (#32)

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,21 @@
 <template>
   <div class="p-5">
-    <app-header />
-    <main class="mt-5">
-      <app-form @add-todo="addTodo" />
-      <app-list
-        :todos="todos"
-        @delete-todo="deleteTodo"
-        @todo-done="todoDone"
-      />
-      <span class="inline-block mt-5">Total todo: {{ totalTodos }}</span>
-    </main>
+    <h1 class="text-4xl">{{ message }}</h1>
   </div>
 </template>
 
 <script>
-import Header from "./components/Fragments/Header.vue";
-import FormInput from "./components/Fragments/Form.vue";
-import ListTodo from "./components/Fragments/ListTodo.vue";
+import { ref } from "vue";
 
 export default {
-  components: {
-    "app-header": Header,
-    "app-form": FormInput,
-    "app-list": ListTodo,
-  },
+  setup() {
+    const message = ref("Hello Jhon Doe");
 
-  data() {
-    return {
-      todos: [],
-    };
-  },
+    setTimeout(() => {
+      message.value = "Hello John Doe";
+    }, 2000);
 
-  created() {
-    this.todos = JSON.parse(localStorage.getItem("todos")) || [];
-  },
-
-  computed: {
-    totalTodos() {
-      return this.todos.length;
-    },
-  },
-
-  methods: {
-    addTodo(value) {
-      this.todos.unshift({
-        title: value,
-        isDone: false,
-      });
-      this.saveToLocalstorage();
-    },
-    deleteTodo(id) {
-      this.todos = this.todos.filter((item, index) => index !== id && item);
-      this.saveToLocalstorage();
-    },
-    todoDone(id) {
-      this.todos = this.todos.filter((item, index) => {
-        index === id && (item.isDone = !item.isDone);
-        return item;
-      });
-      this.saveToLocalstorage();
-    },
-    saveToLocalstorage() {
-      localStorage.setItem("todos", JSON.stringify(this.todos));
-    },
+    return { message };
   },
 };
 </script>
-
-<style></style>
