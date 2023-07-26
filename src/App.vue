56cc03c968fb6e6fcfefdc3b1c6e9f3ff1c337ff
galihroswandi <galihroswandi25@gmail.com>
2023-07-26 14:32:42 +0700

    feat: Video 10 Lifecycle (#40)

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,21 @@
 <template>
-  <div class="p-5">
-    <h4>{{ nilai }}</h4>
-    <button @click="add">Add</button>
-  </div>
+  <div class="p-5"></div>
 </template>
 
 <script>
-import { reactive, toRefs, ref, watch } from "vue";
+import {
+  onBeforeMount,
+  onMouted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  onActivated,
+  onDeactivated,
+  onErrorCaptured,
+} from "vue";
 
 export default {
-  setup() {
-    const count = reactive({
-      nilai: 0,
-      foo: "bar",
-    });
-
-    const num1 = ref(1);
-    const num2 = ref(2);
-
-    const add = () => {
-      count.nilai++;
-      num1.value++;
-      num2.value++;
-    };
-
-    watch([num1, num2], (current, before) => {
-      console.log(num1.value);
-      console.log(current);
-      console.log(before);
-    });
-
-    watch(
-      () => count.nilai,
-      (current, before) => {
-        console.log({ current });
-        console.log({ before });
-      }
-    );
-
-    return { ...toRefs(count), add };
-  },
+  setup() {},
 };
 </script>
